ece62683417dadfd7548577df8b7df546c603840
NathanBanavong <44409112+NathanBanavong@users.noreply.github.com>
2018-12-05 16:31:07 -0500

    Created Barcode Project
    implements 'Timber'

---
+++ Barcode/app/src/main/java/com/example/consultants/week7_daily3/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.consultants.week7_daily3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        plant the timber log 'tree'
+        Timber.plant();
+
+    }
+}
