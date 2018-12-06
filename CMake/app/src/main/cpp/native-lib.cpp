7e1f22738edb707ee0249ebd686b1ebb0a3d6f7d
NathanBanavong <44409112+NathanBanavong@users.noreply.github.com>
2018-12-05 22:14:39 -0500

    C++ to textview

--- CMake/app/src/main/cpp/native-lib.cpp
+++ CMake/app/src/main/cpp/native-lib.cpp
@@ -3,8 +3,20 @@
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_consultants_cmake_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Enter Numbers to Add";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_example_consultants_cmake_MainActivity_showFROMNJI(JNIEnv *env, jobject, jint a,
+                                                            jint b) {
+
+    // TODO
+    std::int16_t sum = a + b;
+    std::to_string(sum);
+    return env->;
+
 }
--- CMake/app/src/main/java/com/example/consultants/cmake/MainActivity.java
+++ CMake/app/src/main/java/com/example/consultants/cmake/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.consultants.cmake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,14 +12,34 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    private TextView tvMain;
+    private TextView tvSum;
+    private EditText etNum2;
+    private EditText etNum1;
+    int num1;
+    int num2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        onBindView();
+        tvMain.setText(stringFromJNI());
+//        int Sum = showFROMNJI(num1, num2);
+//        tvSum.setText(String.valueOf(Sum));
+        tvSum.setText(String.valueOf(showFROMNJI(num1, num2)));
+    }
+
+    public void onBindView() {
+        tvMain = findViewById(R.id.tvMain);
+        tvSum = findViewById(R.id.tvSum);
+        etNum1 = findViewById(R.id.etNum1);
+        etNum2 = findViewById(R.id.etNum2);
+
+        num1 = Integer.parseInt(etNum1.getText().toString());
+        num2 = Integer.parseInt(etNum2.getText().toString());
     }
 
     /**
@@ -26,4 +47,6 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    public native int showFROMNJI(int a, int b);
 }
